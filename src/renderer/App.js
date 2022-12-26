4a5866feb7876fb6659d7a95646157427b755efd
Radek Ripa <ripa.r@seznam.cz>
2022-12-26 19:15:00 +0100

    css correction added loaders for css fixed css selection in webpack

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -6,6 +6,12 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import SetupInfoPanel from "./components/SetupInfoPanel";
 
+/*fonts*/
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 class App extends Component {
   constructor(props) {
     super(props);
